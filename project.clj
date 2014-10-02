5fb434e61eb95a5344aee1c9cf45a5c3e4e39141
Hannu Leinonen <hleinone@gmail.com>
2014-10-02 09:52:23 +0300

    Added license file to project.clj.

--- project.clj
+++ project.clj
@@ -3,5 +3,8 @@
   :dev-dependencies [[swank-clojure "1.4.2"]]
   :dependencies [[org.clojure/clojure "1.3.0"]]
   :lein-release {:deploy-via :clojars}
+  :url "https://github.com/hleinone/lein-uberjar-release"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true
   :min-lein-version "2.0.0")
